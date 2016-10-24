e7770a656066106fc19740d434a622905526f313
AustinMahan <amahan0424@gmail.com>
2016-10-24 14:35:47 -0600

    post request

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -21,6 +21,7 @@
 
 function crossOrigin(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Headers','Content-Type')
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
   next()
 }
